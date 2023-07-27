31b6cb5d9d506870000f5136da5b121ac5d395c8
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2023-07-27 20:21:04 +0300

    updated 2.27-1

--- controllers/orders-controllers.js
+++ controllers/orders-controllers.js
@@ -6,7 +6,6 @@ const { orderOneClickValidation } = require("../models/orderOneclick");
 const { orderValidation } = require("../models/orderBasket");
 const sendEmail = require("../helpers/sendEmail");
 
-
 const addOrderByOneClick = async (req, res) => {
     const {error} = orderOneClickValidation.validate(req.body);
     if(error) {
@@ -39,7 +38,78 @@ const addOrderByBasket = async (req, res) => {
     return res.status(400).json({"message": error.message});
   };
   const result = await OrderBasket.create({...req.body});
-res.status(201).json(result);
+
+  let order = [];
+
+    for (const item of result.confirmedOrder) {
+      const itemsOforder = 
+      `
+      <h3>Назва товару: ${item.title}</h3>
+      <p>Код товару: ${item.code}</p>
+      <p>Ціна за шт: ${item.price} грн</p>
+      <p>Фасовка: ${item.type} грн</p>
+      <p>Колір: ${item.color} грн</p>
+      <p>Кількість: ${item.quantity} грн</p>
+      <p>Вартість: ${(Number(item.price)*Number(item.quantity)).toFixed(2)} грн</p>
+      `
+      order.splice(0,0, itemsOforder);
+    };
+
+  if(result.typeOfDelivery === "Sklad"){
+    const email = {
+      to: "colorfarb@gmail.com",
+      subject: `Нове замовлення ${result.date}`,
+      html: 
+        `<h1>Нове замовлення №${result._id}</h1>
+        <p>Дата: ${result.date}</p>
+        <p>Ім'я клієнта: ${result.customerName}</p>
+        <p>Телефон клієнта: ${result.phone}</p>
+        <p>Тип доставки: Самовивіз</p>
+        <h2>Замовлення</h2>
+        <div>${order}</div>
+        <h3>Загальна сума до сплати: </h3>`
+    }
+  await sendEmail(email);
+  res.status(201).json(result);
+  }
+  else if(result.typeOfDelivery === "Delivery"){
+    const email = {
+      to: "colorfarb@gmail.com",
+      subject: `Нове замовлення ${result.date}`,
+      html: 
+        `<h1>Нове замовлення ${result._id}</h1>
+        <p>Дата: ${result.date}</p>
+        <p>Ім'я клієнта: ${result.customerName}</p>
+        <p>Телефон клієнта: ${result.phone}</p>
+        <p>Тип доставки: Delivery</p>
+        <p>Населений пункт: ${result.locality}</p>
+        <p>Відділення: ${result.branchNumber}</p>
+        <h2>Замовлення</h2>
+        <div>${order}</div>
+        <h3>Загальна сума до сплати: </h3>`
+    }
+    await sendEmail(email);
+    res.status(201).json(result);
+  }
+  else if(result.typeOfDelivery === "Nova Poshta"){
+    const email = {
+      to: "colorfarb@gmail.com",
+      subject: `Нове замовлення ${result.date}`,
+      html: 
+        `<h1>Нове замовлення ${result._id}</h1>
+        <p>Дата: ${result.date}</p>
+        <p>Ім'я клієнта: ${result.customerName}</p>
+        <p>Телефон клієнта: ${result.phone}</p>
+        <p>Тип доставки: Нова пошта</p>
+        <p>Населений пункт: ${result.locality}<p/>
+        <p>Відділення: ${result.branchNumber}</p>
+        <h2>Замовлення</h2>
+        <div>${order}</div>
+        <h3>Загальна сума до сплати: </h3>`
+    }
+    await sendEmail(email);
+    res.status(201).json(result);
+  }
 };
 
 module.exports = {
